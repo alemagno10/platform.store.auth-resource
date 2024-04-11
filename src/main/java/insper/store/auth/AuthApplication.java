b377a8d7c56631c177e6158ef83080c42f9aec00
alemagno10 <alemagno03@gmail.com>
2024-04-11 19:53:17 -0300

    update

--- src/main/java/insper/store/auth/AuthApplication.java
+++ src/main/java/insper/store/auth/AuthApplication.java
@@ -5,14 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-// import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @EnableFeignClients(basePackages = {
     "insper.store.account"
 })
 @EnableDiscoveryClient
 @SpringBootApplication
-// @EnableSwagger2
 public class AuthApplication {
 
     public static void main(String[] args) {
